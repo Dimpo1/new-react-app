711210a6ed5a6c26433c97f0d1bc0284da722e97
Dimpo1 <111021090+Dimpo1@users.noreply.github.com>
2024-04-07 18:02:49 +0200

    correcting errors for deploy

---
+++ src/Appfooter.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Appfooter() {
+  return (
+    <div className="Appfooter">
+      {" "}
+      <footer>
+        Coded by <a href="https://github.com/Dimpo1">Dimpo Phiri</a>, the
+        project is open-sourced on{" "}
+        <a href="https://github.com/Dimpo1/react-weather-app">GitHub</a> and the
+        website is hosted on{" "}
+        <a href="https://dp-react-weatherapp.netlify.app/">Netlify</a>.
+      </footer>
+    </div>
+  );
+}
--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,10 @@
 import React from "react";
 import WeatherData from "./WeatherData";
-import Appforecast from "./Appforecast";
 
 export default function Main() {
   return (
     <main>
       <WeatherData defaultCity="Paris" />
-      <Appforecast />
     </main>
   );
 }
